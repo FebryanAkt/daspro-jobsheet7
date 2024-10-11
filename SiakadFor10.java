417f60ddaf43e65c06f12423fc752362e3c0e568
FebryanAkt <febryan08022006@gmail.com>
2024-10-11 07:38:50 +0700

    Percobaan 1 modifikasi

--- SiakadFor10.java
+++ SiakadFor10.java
@@ -3,7 +3,8 @@ public class SiakadFor10 {
     public static void main (String [] args) {
 
         Scanner sc = new Scanner(System.in);
-        double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 0, terendah = 100, batas = 60;
+        int mhsLulus = 0, mhsTdkLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ":");
@@ -14,8 +15,18 @@ public class SiakadFor10 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= batas) {
+                mhsLulus++;
+            } else {
+                mhsTdkLulus++;
+            }
         }
+        System.out.println("--------------------------------");
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("--------------------------------");
+        System.out.println("Jumlah Mahasiswa yang lulus: " + mhsLulus);
+        System.out.println("Jumlah Mahasiswa yang tidak lulus: " + mhsTdkLulus);
+        System.out.println("--------------------------------");
     }
 }
